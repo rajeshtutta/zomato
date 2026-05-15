b9d6c85bd50921706de08aa1d7d7dfbd82d51592
Mr. Rajesh <rajeshtutta9143@gmail.com>
2026-05-15 12:47:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-import { Provider } from "react-redux";
-import { store } from "./Redux/store";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+import { BrowserRouter }
+from "react-router-dom";
+
+import { Provider }
+from "react-redux";
+
+import { store }
+from "./Redux/store";
+
+const root =
+  ReactDOM.createRoot(
+    document.getElementById("root")
+  );
 
 root.render(
 
