8e5e6037abc100ee67fc4a61ecdda40e6e08189e
Mr. Rajesh <rajeshtutta9143@gmail.com>
2026-05-15 12:43:54 +0530

    Update authActions.js

--- src/Redux/Auth/authActions.js
+++ src/Redux/Auth/authActions.js
@@ -1,6 +1,11 @@
-import { LOGIN_SUCCESS, LOGOUT } from "./authTypes";
+import {
+  LOGIN_SUCCESS,
+  LOGOUT,
+} from "./authTypes";
 
-export const loginSuccess = (payload) => ({
+export const loginSuccess = (
+  payload
+) => ({
   type: LOGIN_SUCCESS,
   payload,
 });
