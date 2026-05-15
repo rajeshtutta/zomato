9fc8a2903ba950aa0dec169d5099727bb2d195bf
Mr. Rajesh <rajeshtutta9143@gmail.com>
2026-05-15 12:44:44 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
+import { useSelector }
+from "react-redux";
+
+import { Navigate }
+from "react-router-dom";
+
+const ProtectedRoute = ({
+  children,
+}) => {
 
   const isAuth = useSelector(
     (store) => store.auth.isAuth
   );
 
   if (!isAuth) {
-    return <Navigate to="/login" />;
+
+    return (
+      <Navigate to="/login" />
+    );
   }
 
   return children;
