8bb1fa06f1e695806d88fa03e3b0a8008ca2f848
Mr. Rajesh <rajeshtutta9143@gmail.com>
2026-05-15 12:37:50 +0530

    Refactor Header component for improved readability

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,58 +1,134 @@
 import React, { useState } from "react";
 import "./Header.scss";
+
 import Logo from "../../assets/images/Zomato-Logo.png";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import blackLogo from "../../assets/images/blackLogo.webp";
+
 import { useDispatch } from "react-redux";
 import { logout } from "../../Redux/Auth/authActions";
 
 const Header = () => {
+
   const [open, setOpen] = useState(false);
 
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+
+    dispatch(logout());
+  };
+
   return (
+
     <div className="header">
+
       <nav>
+
         <span>Get the App</span>
+
         <div className="right">
+
           <span>Investor Relations</span>
+
           <span>Add restaurant</span>
+
           <span>Log in</span>
+
           <span>Sign up</span>
+
+          <button
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+
         </div>
+
       </nav>
-      <div className="hamburger" onClick={() => setOpen(!open)}>
-        {open ? <CloseIcon style={{ color: "black" }} /> : <MenuIcon />}
+
+      <div
+        className="hamburger"
+        onClick={() => setOpen(!open)}
+      >
+
+        {
+          open
+            ? <CloseIcon style={{ color: "black" }} />
+            : <MenuIcon />
+        }
+
       </div>
-      {open && (
-        <div className="sideMenu">
-          <img src={blackLogo} alt="logo" />
-          <div className="innerMenu">
-            <span>Investor Relations</span>
-            <span>Add restaurants</span>
-            <span>Log In</span>
-            <span>Sign Up</span>
+
+      {
+        open && (
+
+          <div className="sideMenu">
+
+            <img src={blackLogo} alt="logo" />
+
+            <div className="innerMenu">
+
+              <span>Investor Relations</span>
+
+              <span>Add restaurants</span>
+
+              <span>Log In</span>
+
+              <span>Sign Up</span>
+
+            </div>
+
           </div>
-        </div>
-      )}
+        )
+      }
+
       <div className="headerContent">
+
         <img src={Logo} alt="logo" />
-        <h3>Discover the best food & drinks in Patna</h3>
+
+        <h3>
+          Discover the best food & drinks in Patna
+        </h3>
+
         <div className="input">
-          <select name="" id="">
-            <option value="Chennai">Chennai</option>
-            <option value="Jaipur">Jaipur</option>
-            <option value="Delhi">Delhi</option>
-            <option value="Mumbai">Mumbai</option>
-            <option value="Kolkata">Kolkata</option>
+
+          <select>
+
+            <option value="Chennai">
+              Chennai
+            </option>
+
+            <option value="Jaipur">
+              Jaipur
+            </option>
+
+            <option value="Delhi">
+              Delhi
+            </option>
+
+            <option value="Mumbai">
+              Mumbai
+            </option>
+
+            <option value="Kolkata">
+              Kolkata
+            </option>
+
           </select>
+
           |
+
           <input
             type="text"
             placeholder="Search for restaurant, cuisine or a dish"
           />
+
         </div>
+
       </div>
+
     </div>
   );
 };
