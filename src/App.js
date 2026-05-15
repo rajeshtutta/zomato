cc6c656b6748ec139846f03e4c7003e70ab0af41
Mr. Rajesh <rajeshtutta9143@gmail.com>
2026-05-15 12:47:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,80 @@ import Register from "./Pages/Register";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
+import {import Header from "./components/Header/Header";
+import "./app.scss";
+import Footer from "./components/Footer/Footer";
+import AccContainer from "./components/AccContainer/AccContainer";
+import CTA from "./components/CTA/CTA";
+import Cities from "./components/Cities/Cities";
+import Collection from "./components/Collections/Collection";
+import Card from "./components/Card/Card";
+
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import NotFound from "./Pages/NotFound";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function Home() {
+
+  return (
+    <>
+      <Header />
+      <Card />
+      <Collection />
+      <Cities />
+      <CTA />
+      <AccContainer />
+      <Footer />
+    </>
+  );
+}
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
   Routes,
   Route,
 } from "react-router-dom";
