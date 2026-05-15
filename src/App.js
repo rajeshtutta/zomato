c1aa3d78288086cc4cebe3384962300e549b0acf
Mr. Rajesh <rajeshtutta9143@gmail.com>
2026-05-15 14:56:04 +0530

    Remove unused components and clean up imports

--- src/App.js
+++ src/App.js
@@ -9,20 +9,6 @@ import Card from "./components/Card/Card";
 
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-
-import ProtectedRoute from "./components/ProtectedRoute";
-
-import {import Header from "./components/Header/Header";
-import "./app.scss";
-import Footer from "./components/Footer/Footer";
-import AccContainer from "./components/AccContainer/AccContainer";
-import CTA from "./components/CTA/CTA";
-import Cities from "./components/Cities/Cities";
-import Collection from "./components/Collections/Collection";
-import Card from "./components/Card/Card";
-
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
 import NotFound from "./Pages/NotFound";
 
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -77,59 +63,6 @@ function App() {
         <Route
           path="*"
           element={<NotFound />}
-        />
-
-      </Routes>
-
-    </div>
-  );
-}
-
-export default App;
-  Routes,
-  Route,
-} from "react-router-dom";
-
-function Home() {
-
-  return (
-    <>
-      <Header />
-      <Card />
-      <Collection />
-      <Cities />
-      <CTA />
-      <AccContainer />
-      <Footer />
-    </>
-  );
-}
-
-function App() {
-
-  return (
-
-    <div className="App">
-
-      <Routes>
-
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
         />
 
       </Routes>
